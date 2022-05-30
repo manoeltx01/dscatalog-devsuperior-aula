6f3751f8ad78f1f793eba65be7d498bf09ffd82b
Manoel Teixeiragit config --global user.name Manoel <manoeltx01@gmail.com>
2022-05-30 09:52:49 -0300

    CategoryResource Ajustes 01

--- backend/src/main/java/com/devesuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devesuperior/dscatalog/resources/CategoryResource.java
@@ -21,8 +21,8 @@ public class CategoryResource {
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
 		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books)"));
-		list.add(new Category(2L, "Eletronics)"));
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Eletronics"));
 		return ResponseEntity.ok().body(list);
 
 	}
